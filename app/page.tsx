b2adc4ea0a4542f8549efcc021a987eca5f107dd
nicolas <nicogalvei@gmail.com>
2024-05-09 14:02:41 +0200

    lets gooooo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { Carousel } from 'components/carousel';
-import { ThreeItemGrid } from 'components/grid/three-items';
 import Footer from 'components/layout/footer';
 
 export const metadata = {
@@ -12,8 +10,7 @@ export const metadata = {
 export default async function HomePage() {
   return (
     <>
-      <ThreeItemGrid />
-      <Carousel />
+      <div>HELLOOO</div>
       <Footer />
     </>
   );
